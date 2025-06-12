593331c592a2ed8df926ab89bb4680d697e68885
Bruno2204 <brunoegama@gmail.com>
2025-06-11 22:36:15 -0300

    change start players

--- src/hooks/usePlayers.jsx
+++ src/hooks/usePlayers.jsx
@@ -2,25 +2,18 @@ import { useState } from "react";
 import { Player } from "../classes/Player";
 
 export function usePlayers() {
-  const [players, setPlayers] = useState([
-    new Player("Player 1"),
-    new Player("Player 2"),
-  ]);
-
+  const [players, setPlayers] = useState([]);
 
   const addPlayer = (event) => {
     event.preventDefault();
     const { playerName } = Object.fromEntries(
       new window.FormData(event.target)
     );
-    setPlayers((prevPlayers) => [
-      ...prevPlayers,
-      new Player(playerName),
-    ]);
+    setPlayers((prevPlayers) => [...prevPlayers, new Player(playerName)]);
     event.target.reset();
     event.target.playerName.focus();
   };
-  
+
   const resetPlayers = () => {
     setPlayers([]);
   };
